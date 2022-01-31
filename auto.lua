6c0019e09920a5405024f330c7ddd787986ef7b2
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-31 19:40:44 +0100

    Create auto.lua

---
+++ auto.lua
@@ -0,0 +1,141 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-238.36, 5.55, 116.7)
+wait(1)
+local args = {
+    [1] = "bank_dealer",
+    [2] = "Duffel Bag"
+}
+
+game:GetService("ReplicatedStorage")._network.purchase:InvokeServer(unpack(args))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-0.83, -9.38, -25.76))
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-231.31, 5.55, 123.29)
+wait(1)
+local args = {
+    [1] = "bank_dealer",
+    [2] = "Duffel Bag"
+}
+
+game:GetService("ReplicatedStorage")._network.purchase:InvokeServer(unpack(args))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-0.83, -9.38, -25.76))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-229.84, -6.51, 107.18))
+wait(1)
+local args = {
+    [1] = "bank_dealer",
+    [2] = "Duffel Bag"
+}
+
+game:GetService("ReplicatedStorage")._network.purchase:InvokeServer(unpack(args))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-0.83, -9.38, -25.76))
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-229.28, -6.51, 127.75))
+wait(1)
+local args = {
+    [1] = "bank_dealer",
+    [2] = "Duffel Bag"
+}
+
+game:GetService("ReplicatedStorage")._network.purchase:InvokeServer(unpack(args))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-0.83, -9.38, -25.76))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-215.2, -6.51, 114.02))
+wait(1.5)
+local args = {
+    [1] = "bank_dealer",
+    [2] = "Duffel Bag"
+}
+
+game:GetService("ReplicatedStorage")._network.purchase:InvokeServer(unpack(args))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-0.83, -9.38, -25.76))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-220.85, -6.51, 106.68))
+wait(1)
+local args = {
+    [1] = "bank_dealer",
+    [2] = "Duffel Bag"
+}
+
+game:GetService("ReplicatedStorage")._network.purchase:InvokeServer(unpack(args))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-0.83, -9.38, -25.76))
+wait(1)
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("ClickDetector") then
+    fireclickdetector(v)
+    end
+end
+wait(1)
+game.StarterGui:SetCore("SendNotification", {
+Title = "Auto Bank"; -- title of notification
+Text = "Finished"; -- bottom text
+Duration = 5; -- how long notification is on screen
+})
