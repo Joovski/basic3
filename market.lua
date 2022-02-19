5cdee3cedb5a335778831e95fa9ba387d8e520c3
Joovski <53435623+Joovski@users.noreply.github.com>
2022-02-19 02:22:54 +0100

    Update market.lua

--- market.lua
+++ market.lua
@@ -1,1 +1,14 @@
-game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-66.31, 3.47, -194.89))
+local plr = game:service"Players".LocalPlayer;
+local tween_s = game:service"TweenService";
+local info = TweenInfo.new(5,Enum.EasingStyle.Quad);
+function tp(...)
+   local tic_k = tick();
+   local params = {...};
+   local cframe = CFrame.new(params[1],params[2],params[3]);
+   local tween,err = pcall(function()
+       local tween = tween_s:Create(plr.Character["HumanoidRootPart"],info,{CFrame=cframe});
+       tween:Play();
+   end)
+   if not tween then return err end
+end
+tp(-66.31, 3.47, -194.89);
