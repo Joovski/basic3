a7ae14c7d588e34a11561c4bf98d9b642568305b
Joovski <53435623+Joovski@users.noreply.github.com>
2022-02-19 02:22:29 +0100

    Update bank.lua

--- bank.lua
+++ bank.lua
@@ -1,1 +1,14 @@
-game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-245.8, 5.45, 94.48))
+local plr = game:service"Players".LocalPlayer;
+local tween_s = game:service"TweenService";
+local info = TweenInfo.new(5,Enum.EasingStyle.Quad);
+function tp(...)
+   local tic_k = tick();
+   local params = {...};
+   local cframe = CFrame.new(params[1],params[2],params[3]);
+   local tween,err = pcall(function()
+       local tween = tween_s:Create(plr.Character["HumanoidRootPart"],info,{CFrame=cframe});
+       tween:Play();
+   end)
+   if not tween then return err end
+end
+tp(-245.8, 5.45, 94.48);
