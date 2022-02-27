acc0851344d71d74145d6d5324760c05df9773cd
Joovski <53435623+Joovski@users.noreply.github.com>
2022-02-27 07:35:21 +0100

    Update and rename bypass.lua to bypassA.lua

--- bypass.lua
+++
@@ -1,9 +0,0 @@
-game:GetService"RunService".RenderStepped:Connect(function()
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoid = character.Humanoid
-
-humanoid.Seated:Connect(function(active, seat)
-    humanoid:ChangeState(Enum.HumanoidStateType.Running)
-end)
-end)
